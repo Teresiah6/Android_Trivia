30cca522fc22bf516348ebe30ae1b7c91a79396f
Teresiah6 <wangariteresiah6@gmail.com>
2021-06-10 12:53:17 +0300

    safe arguments and share
    Implementation of safe args and sharing using implicit intents

--- app/src/main/java/com/example/android/navigation/GameOverFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameOverFragment.kt
@@ -35,7 +35,7 @@ class GameOverFragment : Fragment() {
 
 
         binding.tryAgainButton.setOnClickListener {view:View ->
-            view.findNavController().navigate(action_gameOverFragment_to_gameFragment)
+            view.findNavController().navigate(GameOverFragmentDirections.actionGameOverFragmentToGameFragment())
         }
         return binding.root
 
